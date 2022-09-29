c8f65ecb3e808377cc78d14b5a0f77b18a13d6f7
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-09-29 02:06:04 -0300

    feat: styled components, deck1 & header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,29 @@
+import logo from "../assets/images/logo.png";
+import styled from "styled-components";
+
 export default function Header() {
-	return <></>;
+	return (
+		<HeaderContainer>
+			<img src={logo} alt="Logo do ZapRecall" />
+			<h1>ZapRecall</h1>
+		</HeaderContainer>
+	);
 }
+
+const HeaderContainer = styled.div`
+	display: flex;
+	align-items: center;
+	margin: 40px 0 20px 0;
+	img {
+		width: 52px;
+	}
+	h1 {
+		font-family: "Righteous";
+		font-style: normal;
+		font-weight: 400;
+		font-size: 36px;
+		line-height: 45px;
+		color: #ffffff;
+		margin-left: 20px;
+	}
+`;
--- src/components/ZapMain.js
+++ src/components/ZapMain.js
@@ -1,13 +1,26 @@
 import Deck from "./Deck";
 import Footer from "./Footer";
 import Header from "./Header";
+import styled from "styled-components";
 
 export default function ZapMain() {
 	return (
-		<>
+		<Main>
 			<Header />
 			<Deck />
 			<Footer />
-		</>
+		</Main>
 	);
 }
+
+const Main = styled.div`
+	background-color: #fb6b6b;
+	width: 100vw;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 0px;
+	padding: 0px;
+	padding-bottom: 200px;
+`;
